6238597c2513e7a401bfd6a3ce6ca94e03eca388
Christopher 'm4z' Holm <them4z@googlemail.com>
2012-06-02 03:31:17 +0200

    Fiddling.

--- src/lib/Bcfg2/Client/Tools/Zypper.py
+++ src/lib/Bcfg2/Client/Tools/Zypper.py
@@ -8,7 +8,6 @@ class Zypper(Bcfg2.Client.Tools.PkgTool):
     """zypper package support."""
     name = 'Zypper'
     __execs__ = ["/bin/rpm", "/usr/bin/zypper"]
-    # TODO also handle files?
     __handles__ = [('Package', 'zypper'),
                    ('Package', 'yum'),
                    ('Package', 'rpm'),
@@ -36,18 +35,29 @@ class Zypper(Bcfg2.Client.Tools.PkgTool):
                         for entry in struct \
                         if entry.tag == 'Path' and \
                         entry.get('type') == 'ignore']
+        # Get the list of currently installed packages.
         self.installed = {}
         self.RefreshPackages()
+        # Get the list of the most recent available packages.
+        self.available = {}
+        self.RefreshPackagesLocally()
 
     def RefreshPackages(self):
-        """Get list of currently installed packages."""
-        self.logger.info("Zypper: Begin Refresh")
+        """Create self.installed, the list of currently installed packages.
+
+           Format:
+               self.installed[name] = [ {'name':'...', 'version':'...',
+                                         'release':'...', 'arch':'...'},
+                                        {...} ]
+        """
+        self.logger.debug("Zypper: Begin Refresh")
         pkgcache = self.cmd.run("/bin/rpm --query --all")[1]
         self.installed = {}
         for pkg in pkgcache:
             # format: <name-with-optional-dashes>-<version>-<release>.<arch>
             pkgname = pkg.rsplit('-', 2)[0]
             version = pkg.rsplit('-', 2)[1]
+
             arch = None
             try:
                 arch = pkg.rsplit('.', 1)[1]
@@ -55,15 +65,55 @@ class Zypper(Bcfg2.Client.Tools.PkgTool):
             except IndexError:
                 release = pkg.rsplit('-', 2)[2]
 
-            #if arch is not None:
-            #    self.logger.debug("Zypper: pkg:     p:%s  v:%s  r:%s  (a:%s)" %
-            #                      (pkgname, version, release, arch))
-            #else:
-            #    self.logger.debug("Zypper: gpg-pkg: p:%s  v:%s  r:%s" %
-            #                      (pkgname, version, release))
+            currentpkg = {}
+            currentpkg['name'] = pkgname
+            currentpkg['version'] = version
+            currentpkg['release'] = release
+            if arch is not None:
+                #self.installed[pkgname] = version + '-' + release + '.' + arch
+                currentpkg['arch'] = arch
+                #self.logger.debug("Zypper: pkg:     p:%s  v:%s  r:%s  (a:%s)" %
+                #                  (pkgname, version, release, arch))
+            else:
+                #self.installed[pkgname] = version + '-' + release
+                currentpkg['arch'] = 'noarch'
+                #self.logger.debug("Zypper: gpg-pkg: p:%s  v:%s  r:%s" %
+                #                  (pkgname, version, release))
+            self.installed.setdefault(pkgname, []).append(currentpkg)
+            #self.logger.debug("Zypper:    %s" % currentpkg)
+        self.logger.debug("Zypper: End Refresh")
 
-            self.installed[pkgname] = version
-        self.logger.info("Zypper: End Refresh")
+    # TODO this takes a moment, so maybe we should log an info msg.
+    def RefreshPackagesLocally(self):
+        """Get list of newest available packages."""
+        self.logger.debug("Zypper: Begin local Refresh")
+        # Force a refresh now, because depending on the zypper configuration,
+        # the metadata might be old.
+        refreshnow = self.cmd.run("/usr/bin/zypper --quiet --non-interactive \
+                                  refresh --force")
+        updates_available = \
+                self.cmd.run("/usr/bin/zypper --quiet --non-interactive \
+                             list-updates --type package")[1]
+        if not self.available:
+            self.available = {}
+        for update in updates_available:
+            # we need to skip the header
+            if update.startswith('S |') or update.startswith('--+'):
+                pass
+            else:
+                # Returns "status | repo description | pkgname | \
+                #          oldversion-release | newversion-release | arch"
+                try:
+                    _, _, pkgname, _, newversionrelease, arch = update.strip().split('|')
+                except ValueError:
+                    self.logger.info("Zypper: got wrong data")
+                pkgname = pkgname.strip()
+                newversionrelease = newversionrelease.strip()
+                arch = arch.strip()
+                self.available[pkgname] = newversionrelease + '.' + arch
+                self.logger.debug("Zypper: Update to v:%s available for %s" %
+                                 (self.available[pkgname], pkgname))
+        self.logger.debug("Zypper: End local Refresh")
 
     def VerifyPackage(self, entry, modlist):
         """Verify Package status for entry, by comparing the versions the server
@@ -78,7 +128,7 @@ class Zypper(Bcfg2.Client.Tools.PkgTool):
         #    # attribs are: name, priority, version, type, uri
         #    for a in ['version', 'type']:
         #        self.logger.debug("Zypper: %s=%s" % (a, entry.get(a)))
-        self.logger.debug("Zypper: Verify: %s (t:%s  v:%s)" %
+        self.logger.debug("Zypper: Verify: %s (t:%s v:%s)" %
                           (entry.get('name'),
                            entry.get('type'),
                            entry.get('version')))
@@ -101,8 +151,16 @@ class Zypper(Bcfg2.Client.Tools.PkgTool):
             elif entry.get('version') == 'auto':
                 # TODO what has to be done here?
                 # TODO get most recent version of packages?
-                self.logger.debug("Zypper: Verify: %s is version=auto" %
-                                  entry.get('name'))
+                if entry.get('name') in self.available:
+                    self.logger.debug("Zypper: Verify: update available" + \
+                                      " for %s: %s -> %s" %
+                                      (entry.get('name'),
+                                       #self.installed[entry.get('name')].index('version'),
+                                       self.installed[entry.get('name')],
+                                       self.available[entry.get('name')]))
+                #else:
+                #   self.logger.debug("Zypper: Verify: no update for %s" %
+                #                      entry.get('name'))
                 return False
 
             else:
@@ -133,8 +191,32 @@ class Zypper(Bcfg2.Client.Tools.PkgTool):
     #    self.RefreshPackages()
     #    self.extra = self.FindExtraPackages()
 
-    #def Install(self, packages, states):
+    def Install(self, packages, states):
+        pass
     #    install_pkgs = []
     #    for pkg in packages:
     #        if ++needs_change++:
     #            install_pkgs.append(pkg)
+
+    def FindExtraPackages(self):
+        """Find extra packages."""
+        pass
+        packages = [e.get('name') for e in self.getSupportedEntries()]
+        extras = []
+
+        for p in list(self.installed.keys()):
+            if p not in packages:
+                entry = Bcfg2.Client.XML.Element('Package', name=p,
+                                                 type=self.pkgtype)
+                for i in self.installed[p]:
+                    inst = Bcfg2.Client.XML.SubElement(entry,
+                                                       'Instance',
+                                                       version=i['version'],
+                                                       release=i['release'],
+                                                       arch=i['arch'])
+                extras.append(entry)
+        return extras
+
+    def VerifyPath(self, entry, _):
+        """Do nothing here since we only verify Path type=ignore"""
+        return True
